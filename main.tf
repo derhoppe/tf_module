080866df24d3b9c9ca343ab15913826507eca96d
derhoppe <sebastian@sebastianhoppe.com>
2023-08-17 07:20:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,10 @@ resource "azurerm_bastion_host" "this" {
   name                = "${var.vnet_name}-bastion"
   location            = var.location
   resource_group_name = var.resource_group_name
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.this[0].id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
 
-  subnet_id         = azurerm_subnet.this[0].id
-  public_ip_address_id = azurerm_public_ip.bastion.id
 }
